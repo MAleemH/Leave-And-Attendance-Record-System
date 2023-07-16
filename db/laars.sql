9e98a2d8f12052ba0877cf0c1865bbea8a4a9b09
MAleemH <aleemhashmi4321@gmail.com>
2023-07-16 10:48:22 +0500

    Updated db

--- db/laars.sql
+++ db/laars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2023 at 09:04 AM
+-- Generation Time: Jul 16, 2023 at 07:46 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -18,8 +18,73 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `online_test`
+-- Database: `laars`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applied_leaves`
+--
+
+CREATE TABLE `applied_leaves` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `leave_type` varchar(50) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `leave_from` date DEFAULT NULL,
+  `leave_to` date DEFAULT NULL,
+  `attachment` text DEFAULT NULL,
+  `status` enum('Pending','Approved','Rejected') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `attendance_date` date DEFAULT NULL,
+  `time_in` time DEFAULT NULL,
+  `time_out` time DEFAULT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `user_id`, `attendance_date`, `time_in`, `time_out`, `status`) VALUES
+(1, 5, '2023-07-15', '08:00:00', '17:00:00', 'Present'),
+(2, 5, '2023-07-16', '08:00:00', '17:00:00', 'Present'),
+(3, 5, '2023-07-01', '08:00:00', '17:00:00', 'Present'),
+(4, 5, '2023-06-01', '08:00:00', '17:00:00', 'Present');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaves`
+--
+
+CREATE TABLE `leaves` (
+  `id` int(10) NOT NULL,
+  `leave_description` varchar(255) NOT NULL,
+  `leave_total` int(100) NOT NULL,
+  `leave_from` date NOT NULL,
+  `leave_to` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `leaves`
+--
+
+INSERT INTO `leaves` (`id`, `leave_description`, `leave_total`, `leave_from`, `leave_to`) VALUES
+(2, 'Casual Leave', 10, '2023-07-14', '2023-12-31'),
+(3, 'Medical Leave', 20, '2023-07-14', '2023-12-31');
 
 -- --------------------------------------------------------
 
@@ -44,11 +109,31 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_image`, `user_address`, `user_contact`, `user_email`, `user_password`, `user_role`) VALUES
 (1, 'Muhammad Aleem', 'IMG_2424.jpg', 'Mirpur AJ&K', '03445387545', 'aleemhashmi4321@gmail.com', 'aleem', 'Admin'),
-(2, 'Umara Hassan', '15276.jpg', 'Mirpur', '03431153025', 'umara@gmail.com', 'umara', 'User');
+(5, 'Mohsin Akram', 'meme.png', 'Jatlan, Mirpur AJK', '03431122332', 'mohsin@gmail.com', 'mohsin', 'Employee');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `applied_leaves`
+--
+ALTER TABLE `applied_leaves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +146,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `applied_leaves`
+--
+ALTER TABLE `applied_leaves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `leaves`
+--
+ALTER TABLE `leaves`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `applied_leaves`
+--
+ALTER TABLE `applied_leaves`
+  ADD CONSTRAINT `applied_leaves_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
