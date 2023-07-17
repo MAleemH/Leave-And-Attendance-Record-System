b6ec33b1173c52df400c0478c642c405abe2e9f6
Your Name <you@example.com>
2023-07-17 14:48:31 +0500

    Displayed leave record to employee

--- employee/attendance.php
+++ employee/attendance.php
@@ -33,7 +33,8 @@ include '../includes/user_sidebar.php';
                 <form action="" method="POST">
                     <div class="mb-3">
                         <label class="form-label fw-bold">Year:</label>
-                        <input type="number" class="form-control w-25" name="year" min="2000" max="2099" placeholder="2023" required>
+                        <input type="number" class="form-control w-25" name="year" min="2000" max="2099"
+                            placeholder="2023" required>
                     </div>
                     <div class="mb-3">
                         <button type="submit" class="btn btn-primary" name="yearly">View Attendance</button>
@@ -57,9 +58,9 @@ include '../includes/user_sidebar.php';
 
                     // Query to fetch attendance for the selected month and employee
                     $sql = "SELECT DAY(attendance_date) AS day, status FROM attendance
-                            WHERE user_id = $employeeId
-                            AND YEAR(attendance_date) = $selectedYear
-                            AND MONTH(attendance_date) = $selectedMonth";
+            WHERE user_id = $employeeId
+            AND YEAR(attendance_date) = $selectedYear
+            AND MONTH(attendance_date) = $selectedMonth";
 
                     $result = mysqli_query($connection, $sql);
 
@@ -67,23 +68,47 @@ include '../includes/user_sidebar.php';
                         echo "<h5>Monthly Attendance - " . date("F Y", strtotime($selectedDate)) . "</h5>";
                         // Create an empty attendance array for all days in the month
                         $attendanceData = array_fill(1, $lastDayOfMonth, 'Absent');
-                
+
                         // Populate the attendance array with fetched data
                         while ($row = mysqli_fetch_assoc($result)) {
                             $attendanceData[$row['day']] = $row['status'];
                         }
-                
+
+                        // Query to fetch leave records for the selected month and employee
+                        $leaveSql = "SELECT DAY(leave_from) AS day, DAY(leave_to) AS day_to FROM applied_leaves
+                     WHERE user_id = $employeeId
+                     AND status = 'Approved'
+                     AND YEAR(leave_from) = $selectedYear
+                     AND MONTH(leave_from) = $selectedMonth";
+
+                        $leaveResult = mysqli_query($connection, $leaveSql);
+
+                        if (mysqli_num_rows($leaveResult) > 0) {
+                            // Update the attendanceData array to show "On leave" for leave days
+                            while ($leaveRow = mysqli_fetch_assoc($leaveResult)) {
+                                $leaveFrom = $leaveRow['day'];
+                                $leaveTo = $leaveRow['day_to'];
+
+                                // Store all "On leave" days for each leave entry
+                                for ($day = $leaveFrom; $day <= $leaveTo; $day++) {
+                                    if ($day >= 1 && $day <= $lastDayOfMonth) {
+                                        $attendanceData[$day] = 'On leave';
+                                    }
+                                }
+                            }
+                        }
+
                         // Display the attendance table
                         echo "<div class='monthly'>";
-                
                         // Generate headers for the days in the selected month
                         for ($day = 1; $day <= $lastDayOfMonth; $day++) {
                             $attendanceStatus = $attendanceData[$day];
-                            echo '<div class="' . ($attendanceStatus == 'Present' ? 'present' : 'absent') . ' monthly-item">';
+                            $statusClass = ($attendanceStatus == 'Present') ? 'present' : (($attendanceStatus == 'On leave') ? 'on-leave' : 'absent');
+                            echo '<div class="' . $statusClass . ' monthly-item">';
                             echo $day . '<br>' . $attendanceStatus;
                             echo '</div>';
                         }
-                
+
                         echo "</div>";
                     } else {
                         echo "No attendance records found for the selected month.";
@@ -96,10 +121,10 @@ include '../includes/user_sidebar.php';
 
                     // Query to fetch attendance for the selected year and employee
                     $sql = "SELECT MONTH(attendance_date) AS month, DAY(attendance_date) AS day, status FROM attendance
-                            WHERE user_id = $employeeId
-                            AND YEAR(attendance_date) = $selectedYear
-                            ORDER BY attendance_date"; // Sort records by date
-
+            WHERE user_id = $employeeId
+            AND YEAR(attendance_date) = $selectedYear
+            ORDER BY attendance_date"; // Sort records by date
+                
                     $result = mysqli_query($connection, $sql);
 
                     if (mysqli_num_rows($result) > 0) {
@@ -115,6 +140,28 @@ include '../includes/user_sidebar.php';
                             $attendanceData[$month][$day] = $status;
                         }
 
+                        // Query to fetch leave records for the selected year and employee
+                        $leaveSql = "SELECT MONTH(leave_from) AS month, DAY(leave_from) AS day, DAY(leave_to) AS day_to FROM applied_leaves
+                        WHERE user_id = $employeeId
+                        AND status = 'Approved'
+                        AND YEAR(leave_from) = $selectedYear";
+
+                        $leaveResult = mysqli_query($connection, $leaveSql);
+
+                        if (mysqli_num_rows($leaveResult) > 0) {
+                            // Update the attendanceData array to show "On leave" for leave days
+                            while ($leaveRow = mysqli_fetch_assoc($leaveResult)) {
+                                $leaveMonth = $leaveRow['month'];
+                                $leaveFrom = $leaveRow['day'];
+                                $leaveTo = $leaveRow['day_to'];
+
+                                // Store all "On leave" days for each leave entry
+                                for ($day = $leaveFrom; $day <= $leaveTo; $day++) {
+                                    $attendanceData[$leaveMonth][$day] = 'On leave';
+                                }
+                            }
+                        }
+
                         // Display the attendance tables for each month
                         for ($month = 1; $month <= 12; $month++) {
                             echo "<h5>" . date("F", mktime(0, 0, 0, $month, 1)) . " " . $selectedYear . "</h5>";
@@ -125,7 +172,8 @@ include '../includes/user_sidebar.php';
                             // Loop through all days in the month
                             for ($day = 1; $day <= cal_days_in_month(CAL_GREGORIAN, $month, $selectedYear); $day++) {
                                 $attendanceStatus = isset($attendanceData[$month][$day]) ? $attendanceData[$month][$day] : 'Absent';
-                                echo '<div class="' . ($attendanceStatus == 'Present' ? 'present' : 'absent') . ' monthly-item">';
+                                $statusClass = ($attendanceStatus == 'Present') ? 'present' : (($attendanceStatus == 'On leave') ? 'on-leave' : 'absent');
+                                echo '<div class="' . $statusClass . ' monthly-item">';
                                 echo $day . '<br>' . $attendanceStatus;
                                 echo '</div>';
                             }
